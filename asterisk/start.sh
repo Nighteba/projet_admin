aa56ee3dc86a3766ac6e8065df5640f216702762
HE201408 <HE201408@students.ephec.be>
2017-04-30 22:28:56 +0200

    asterisk suite
    suite

--- asterisk/start.sh
+++ asterisk/start.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
-apt-get install build-essential wget libssl-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) libsqlite3-dev uuid-dev git subversion
-cd /usr/src
+mkdir -p /usr/src/asterisk
+cd /usr/src/asterisk
 wget downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
-tar zxvf asterisk-13-current.tar.gz
+tar -zxvf asterisk-13-current.tar.gz
 rm -f asterisk-13-current.tar.gz
-cd asterisk-13*
-git clone git://github.com/asterisk/pjproject pjproject
-cd pjproject
+cd asterisk-13.*
+./configure  --with-resample --with-pjproject-bundled
+make menuselect/menuselect menuselect-tree menuselect.makeopts
+# disable BUILD_NATIVE to avoid platform issues
+menuselect/menuselect --disable BUILD_NATIVE menuselect.makeopts
 
-./configure --prefix=/usr --enable-shared --disable-sound --disable-resample --disable-video --disable-opencore-amr CFLAGS='-O2 -DNDEBUG'
+# enable good things
+menuselect/menuselect --enable BETTER_BACKTRACES menuselect.makeopts
+menuselect/menuselect --enable codec_opus menuselect.makeopts
 
-make dep
-make && make install
-ldconfig
-ldconfig -p |grep pj
-cd ..
-contrib/scripts/get_mp3_source.sh
-contrib/scripts/install_prereq install
-./configure && make menuselect && make && make install
-make samples
-make config
-ldconfig
-/etc/init.d/asterisk start
+# download more sounds
+for i in CORE-SOUNDS-EN MOH-OPSOUND EXTRA-SOUNDS-EN; do
+    for j in ULAW ALAW G722 GSM SLN16; do
+        menuselect/menuselect --enable $i-$j menuselect.makeopts
+    done
+done
+
+make
+make install
+chown -R asterisk:asterisk /var/*/asterisk
+chmod -R 755 /var/spool/asterisk
+mkdir -p /etc/asterisk/
+
+# copy default configs
+cp /usr/src/asterisk/configs/basic-pbx/*.conf /etc/asterisk/
+
+# set runuser and rungroup
+sed -i -E 's/^;(run)(user|group)/\1\2/' /etc/asterisk/asterisk.conf
+
+cd /
+exec rm -rf /usr/src/asterisk
+
 
 #service asterisk start
 #tail -f /var/log/dmesg
