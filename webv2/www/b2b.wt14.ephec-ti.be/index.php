794033e8e33377145962cb82370f4ba98d932b21
HE201408 <HE201408@students.ephec.be>
2017-05-13 17:00:43 +0200

    Update index.php

--- webv2/www/b2b.wt14.ephec-ti.be/index.php
+++ webv2/www/b2b.wt14.ephec-ti.be/index.php
@@ -2,15 +2,15 @@
 include ('dbConnect.inc.php');
 include ('mesFonction.inc.php');
 
-    $dbName = '1617he201408';
-    $sql = "select * from $dbName.produit";
+    //$dbName = 'shop';
+    $sql = "select * from shop.produit";
 
     try {
-        $dbh = new PDO('mysql:host='.getServer().';dbname='.$dbName, 'PETRE', 'SamuelFv77');
+				$dbh = new PDO('mysql:host=172.17.0.5;dbname=shop','root',);
+        //$dbh = new PDO('mysql:host='.getServer().';dbname='.$dbName, 'PETRE', 'SamuelFv77');
         $donnee = $dbh->query($sql);
                                 $donnee->execute();
                                 echo(creeTableau($donnee->fetchAll(PDO::FETCH_ASSOC), 'Liste des produits', true));
-                                $donnee -> closeCursor();
         $dbh = null;
     }
     catch (PDOException $e) {
