1d8e85d93c84f2f32884c042519400e7bffa4e9d
HE201408 <HE201408@students.ephec.be>
2017-05-27 21:13:56 +0200

    Update index.php

--- webv2/www/b2b.wt14.ephec-ti.be/index.php
+++ webv2/www/b2b.wt14.ephec-ti.be/index.php
@@ -1,12 +1,14 @@
 <?php
-include ('dbConnect.inc.php');
 include ('mesFonction.inc.php');
 
     //$dbName = 'shop';
-    $sql = "select * from shop.produit";
+    $sql = "select * from produit";
+    $dbname = "woody";
+    $host="79.137.38.241"
 
     try {
-				$dbh = new PDO('mysql:host=172.17.0.5;dbname=shop','root',);
+	//$dbh = new PDO('mysql:host='.$host.';dbname='.$dbname.,'root','vulcain');
+	$dbh = new PDO('mysql:host=79.137.38.241;dbname=woody','root','vulcain');
         //$dbh = new PDO('mysql:host='.getServer().';dbname='.$dbName, 'PETRE', 'SamuelFv77');
         $donnee = $dbh->query($sql);
                                 $donnee->execute();
